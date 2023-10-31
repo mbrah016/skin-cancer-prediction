d260978f061166a90a5f176e83cbe03c26887f03
Manish Brahmbhatt <124465774+mbrah016@users.noreply.github.com>
2023-10-30 23:43:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,10 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+# Allow any origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -61,6 +58,6 @@ async def predict(file: UploadFile = File(...)):
         'confidence': float(confidence)
     }
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
